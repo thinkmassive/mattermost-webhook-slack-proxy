c0ba56bccde3999a0398840cfc379dc70b312707
Alex Miller <alex.miller@anchore.com>
2021-05-07 10:40:27 -0400

    attempt to handle multiple blocks, drop ">>>" in message (for Anchore notifications)

--- proxy.py
+++ proxy.py
@@ -7,7 +7,11 @@ def request(flow):
     if "application/json" in flow.request.headers["content-type"]:
         slack_json = json.loads(flow.request.text)
         if "blocks" in slack_json:
-            mattermost_json = '{"text": "' + slack_json['blocks'][0]['text']['text'] + '"}'
+            slack_text = slack_json['blocks'][0]['text']['text'].replace(">>>","")
+            if len(slack_json['blocks']) > 1:
+                for block in slack_json['blocks']:
+                    slack_text += "\n" + slack_json['blocks'][block]['text']['text']
+            mattermost_json = '{"text": "' + slack_text + '"}'
         else:
             mattermost_json = slack_json
 
